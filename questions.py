998d61b4be8533c1a8c6255e407057156c0adcdd
Vicki <vicki.langer@statalog.org>
2020-01-21 14:32:41 -0500

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,14 +1,18 @@
 '''
-questions.py: questions to be used by tweet.py
+questions.py: questions to be used by tweet.py for @CodeQBot
 21 January 2020
 Vicki Langer (@vicki_langer)
 '''
 
 # TODO: add more questions
+# NOTE: please add the question, then comment the expected answer
 
 QUESTIONS = [
-  'Sample question?',
-  'Another sample question?',
-  'Here\'s a sample question?',
-  'What\'s another sample question?',
-  ]
+    'Who has been called the worlds first computer programmer?',  # Ada Lovelace
+    'Who popularized the idea of machine-independent programming languages?',  # Grace Hopper
+    'What is "!" used for in "!=" ?',  # Not
+    'What is required in the HTML <head>?',  # charset, viewport, title https://htmlhead.dev
+    'What’s difference between The Internet and The Web?',  # hardware vs software
+    'What characters do you use to comment your code? What language are you using?',  # Depends on the language
+    'What\'s another question?',
+    ]
