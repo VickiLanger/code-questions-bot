63a7455b19316f35311d3bd4cf1840fbd80a67af
Vicki <vicki.langer@statalog.org>
2020-01-21 13:46:31 -0500

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,14 @@
+'''
+questions.py: questions to be used by tweet.py
+21 January 2020
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more questions
+
+QUESTIONS = [
+  'Sample question?',
+  'Another sample question?',
+  'Here\'s a sample question?',
+  'What\'s another sample question?',
+  ]
