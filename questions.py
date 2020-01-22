79f34147efff348701a45eb77f72038de20f635a
Vicki <vicki.langer@statalog.org>
2020-01-22 16:58:22 -0500

    Added some trivia-esque questions

--- questions.py
+++ questions.py
@@ -14,6 +14,12 @@ QUESTIONS = [
     'What is required in the HTML <head>?',  # charset, viewport, title https://htmlhead.dev
     'What’s difference between The Internet and The Web?',  # hardware vs software
     'What characters do you use to comment your code? What language are you using?',  # Depends on the language
+    'Why does List use () instead of []?',  # The keyboard used to develop had issues with the [ key
+    'What does Wi-Fi stand for?',  # nothing https://www.webopedia.com/DidYouKnow/Computer_Science/wifi_explained.asp
+    'What is 0.1 + 0.2 in your language? Why?',  # https://0.30000000000000004.com/
+    'ELI5: What is the DOM?',  # https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+    'ELI5: What are the APIs?',  # https://developer.mozilla.org/en-US/docs/Web/API 
+    '#',  # JavaScript has both +0 and -0
     'Answer with a GIF. \n What is HTTP status 404?',  # Not Found https://httpstatuses.com
     'Answer with a GIF. \n What is HTTP status 200?',  # Okay https://httpstatuses.com
     'Answer with a GIF. \n What is HTTP status 308?',  # Permanent Redirect https://httpstatuses.com
