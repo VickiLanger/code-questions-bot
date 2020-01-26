36a0cd60e95637b24dc18cdce932fe1a28a63c60
Vicki <vicki.langer@statalog.org>
2020-01-26 16:48:58 -0500

    hoping this test works

--- tweet.py
+++ tweet.py
@@ -23,12 +23,18 @@ api = tweepy.API(auth)
 
 def main():
     interval = 60 * 60 * 6  # seconds * minutes * hours
+    reply_with = 'Did it work? \nIt did if you see this!!!!'  # 'Here\'s our question! Stick around, we\'ll have another in 6 hours! \nIt may be easy, it may be hard. \nEither way, give it a tweet \nDon\'t know it? Look it up \nStill unsure? Ask'
+
     while True:
         print("finding a question...")
         question = get_question()
         print("chose question: " + question)
         api.update_status(question)
         print('question has been tweeted')
+        # capture id of that tweet: api.user_timeline(screen_name = toReply, count=1)
+        api.user_timeline('@CodeQBot' = toReply, count=1)
+        # reply to that tweet: API.update_status(reply_with, '@CodeQBot')
+        API.update_status(reply_with, '@CodeQBot')
         time.sleep(interval)
 
 
