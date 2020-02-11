d0f5c65cc931f4f72da1283adbbc42cc4dc46958
Vicki <vicki.langer@statalog.org>
2020-02-11 15:09:33 -0500

    organize questions

---
+++ questions_misc.py
@@ -0,0 +1,78 @@
+'''
+questions_misc.py: miscellaneous questions for @CodeQBot
+11 February 2020
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more questions
+# NOTE: please add the question, then comment the expected answer
+
+misc_questions = [
+    'What’s difference between The Internet and The Web?',  # hardware vs software
+    'What characters do you use to comment your code? What language are you using?',  # Depends on the language
+    'ELI5: What is the DOM?',  # https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+    'ELI5: What are the APIs?',  # https://developer.mozilla.org/en-US/docs/Web/API
+    'What is the name of the Java mascot?',  # Duke
+    'In a tree, where is the root?',  # The top
+    'In terms of coding: when may a parent kill a child?',  # if the task assigned to them is no longer needed
+    'What is recursion?',  #
+    'ELI5: What is cache?',  #
+    'ELI5: What is an IDE?',  #
+    'What is a code smell?',  #
+    'ELI5: What is cookies?',  #
+    'What are comments for?',  #
+    'ELI5: What is the cloud?',  #
+    'What is the "black box?"',  #
+    'ELI5: What is debugging?',  #
+    'ELI5: "=" != "==" != "==="',  #
+    'What does back end refer to?',  #
+    'What does database refer to?',  #
+    'What is JSON? What is it for?',  #
+    'What does front end refer to?',  #
+    'What is Open Source Software (OSS)?',  #
+    'What is pseudocode? Why would you use it',  #
+    'Whats the difference between an editor and an IDE?',  #
+    'What is the difference between Java and JavaScript?',  #
+    'What is the difference between a framework and library?',  #
+    'What can database devs do to make things more accessible?',  #
+    'What is clean code? How do you make sure you code is clean?',  #
+    'What is the difference between computer science and programming?',  #
+    'What is the difference between front end, back end, and database?',  #
+    'What is the difference between high-level and low-level languages?',  #
+    'Who are the "Big 4" Would you work for them?',  # some combination of Google, Microsoft, Facebook, Amazon, and Apple
+    'What percentage of programming jobs are in industries outside of tech?',  # Currently around 70%
+    'Is JavaScript used for front end or back end?',  # Both
+    'How do you conect the front end with the back end?',  #
+    'What is a headless CMS? Why would I need this?',  # https://dev.to/defman/whats-headless-cms-2fek
+    'What is recursion?',  # https://dev.to/rahatch/chocolate-chip-cookies-and-recursion-566k
+    'What is a computer?',  #
+    'What are cron jobs? What are they for?',  #
+    'What is a compiler? Why would one use it?',  #
+    'What is blockchain? Why would one use it?',  #
+    'What are Service Workers? What are they for?',  #
+    'What are containers?',  # https://dev.to/ashleemboyer/explain-containers-like-i-m-five-4cbo
+    'What are closures?',  # https://dev.to/rahatch/learning-closures-with-pokemon-teams-4109
+    'What is documentation? \nIs it important? \nWhy?',
+    'What is nesting',  #
+    'What is the best way to organize methods/functions within a file?',  # https://dev.to/ben/what-is-the-best-way-to-organize-methods-functions-within-a-file-11ih
+
+    # on the job stuff?
+    'What is a code review?',  #
+    'What is pair programming?',  #
+    'What is Agile?',  #
+    'What is scrum?',  #
+
+    # Job Titles
+    'What is a developer advocate?',  # https://dev.to/simo97/what-is-a-developer-advocate--1jei
+    'What is a developer?',  #
+    'What is a software engineer?',  #
+    'What is a full stack developer?',  #
+    'What is the difference between a software engineer and a developer?',  #
+    'What is the difference between a software engineer and a software architecht?',  #
+    'What is DevOps?',  #
+
+    # Ergonomics
+    'In terms of ergonomics, what angle should your screen be at?',  # 20 degrees
+    'In terms of ergonomics, what angle should your elbows be at?',  # 90 degrees
+    'In terms of ergonomics, how far away from your screen should you be?',  #
+    ]
