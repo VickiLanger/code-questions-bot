93de7425e360802c0ab22cc678fe72012b0cd19e
Vicki <vicki.langer@statalog.org>
2020-02-11 15:09:18 -0500

    different types of questions

--- questions_back_end.py
+++ questions_back_end.py
@@ -8,13 +8,48 @@ Vicki Langer (@vicki_langer)
 # NOTE: please add the question, then comment the expected answer
 
 back_end_questions = [
-    'What does it mean to concatenate?',
-    'What does it mean to loop?',
+    'What is a class?',  #
+    'What is a method',  #
+    'What is a module?',  #
+    'What is a function?',  #
+    'What are data types?',  #
+    'What are data structures?',  #
+    'What are lambda functions?',  #
+    'What does it mean to loop?',  #
+    'What is a dev environment?',  #
+    'What is an attribute of a class?',  #
+    'What does it mean to concatenate?',  #
+    'What is exception handling? Why use it?',  #
+    'ELI5: What is Regular Expression (RegEx)?',  #
+    'What does it mean to be "Turing complete"?',  #
+    'What should you do when a line is too long?',  #
+    'How is exception handling different from a loop?',  #
+    'When would you use WET principles instead of DRY?',  #
+    'When would you use DRY principles instead of WET?',  #
+    'What can back end devs do to make things more accessible?',  #
+    'What is an instance and what does it mean to instantiate?',  #
+    'What is the difference between a "for loop" and a "while loop"?',  #
+    'What are conditionals?',  # if, elif, else https://dev.to/vickilanger/charming-the-python-conditionals-4e5f
+    'What are logical operators?',  # and + or https://dev.to/vickilanger/charming-the-python-conditionals-4e5f
+    'Why does List, the language,  use () instead of []?',  # The keyboard used to develop had issues with the [ key
+    'What does "++++++++++[>+++++++>++++++++++>+++>+<<<<-] >++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." mean? What language is it in?',  # Hello World, Brainfuck
+    'There is a language written with no letters , no numbers just 8 characters : > < + - . , [ ] \nWhat is it?',  # Brainfuckhttps://docs.google.com/document/d/1M51AYmDR1Q9UBsoTrGysvuzar2_Hx69Hz14tsQXWV6M/edit
+    'What language was created for orangutans?',  # Ook
+    'What are the three syntax elements of the language created for orangutans?',  # Ook. Ook! Ook?
+    'What language is made of only spaces, tabs and newlines?',  # Whitespace
+    'How would you print "Hello World" in LOLCODE',  # https://github.com/justinmeza/lolcode-spec/blob/master/v1.2/lolcode-spec-v1.2.md
     'What happens when you try to `import braces` in Python?',  # SyntaxError: not a chance
     'Emojis. Recipes. Music notes. LOLCATS. \nWhich of these is used to create a programming language?',  # All of the above
     'What is "!" used for in "!=" ?',  # Not
     'What is 0.1 + 0.2 in your language? Why?',  # https://0.30000000000000004.com/
     'What is polymorphism?',  # A programming language feature that allows the values of different data types to be handled using a uniform interface.
-    'When would you use WET principles instead of DRY?',  #
-    'When would you use DRY principles instead of WET?',  #
+
+    # Real life examples
+    'What is a real life example of "#"?',  #
+    'What is a real life example of "logical operators"?',  # if raining and dog_is_napping: skip_playing_ball
+    'What is a real life example of "conditionals"?',  # if nice_outside: play_ball_with_dog
+    'What is a real life example of a "function"?',  # run: pick up right for, pick up left foot, propel forward, repeat
+    'What is a real life example of a "class" and some "attributes"?',  # dog: name, colors, age, chipped https://dev.to/vickilanger/charming-the-python-data-structures-154i
+    'What is a real life example of a "for loop"?',  # for each dirty dish, clean dish
+    'What is a real life example of a "while loop"?',  # while not napping, play fetch with dog
     ]
