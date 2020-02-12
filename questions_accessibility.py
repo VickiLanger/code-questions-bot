2f8910b25f62d242af6dbd4febd007c489a58965
Vicki <vicki.langer@statalog.org>
2020-02-12 13:43:03 -0500

    new types of questions

---
+++ questions_accessibility.py
@@ -0,0 +1,21 @@
+'''
+questions_accessibility.py: accessibility questions for @CodeQBot
+11 February 2020
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more questions
+# NOTE: please add the question, then comment the expected answer
+
+accessibility_questions = [
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    '',  #
+    ]
