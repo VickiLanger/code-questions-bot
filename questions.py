2b710d353d204bbff8c2148a97aed6d366a77f2f
Vicki <vicki.langer@statalog.org>
2020-02-14 17:25:03 -0500

    more awesome questions

--- questions.py
+++ questions.py
@@ -8,6 +8,7 @@ from questions_accessibility import accessibility_questions
 from questions_acronyms import acronym_questions
 from questions_back_end import back_end_questions
 from questions_front_end import front_end_questions
+from questions_fun import fun_questions
 from questions_history import history_questions
 from questions_http_statuses import http_status_questions
 from questions_misc import misc_questions
@@ -19,4 +20,4 @@ from questions_version_control import version_control_questions
 # TODO: add more questions
 # NOTE: please add the question, then comment the expected answer
 
-QUESTIONS = accessibility_questions + acronym_questions + back_end_questions + front_end_questions + http_status_questions + history_questions + http_status_questions + misc_questions + personal_questions + pop_culture_questions + version_control_questions
+QUESTIONS = accessibility_questions + acronym_questions + back_end_questions + front_end_questions + fun_questions + http_status_questions + history_questions + http_status_questions + misc_questions + personal_questions + pop_culture_questions + version_control_questions
---
+++ questions_fun.py
@@ -0,0 +1,23 @@
+'''
+questions_fun.py: fun questions for @CodeQBot
+14 February 2020
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more questions
+# NOTE: please add the question, then comment the expected answer
+
+fun_questions = [
+    'Wrong answers only!\nWhat is HTML?\n👇🏼👇🏾👇🏿👇🏻👇🏽',
+    'Wrong answers only!\nWhat is SVG?\n👇🏽👇🏼👇🏾👇🏿👇🏻',
+    'Wrong answers only!\nWhat is programming?\n👇🏻👇🏽👇🏼👇🏾👇🏿',
+    'Wrong answers only!\nWhat is JavaScript?\n👇🏿👇🏻👇🏽👇🏼👇🏾',
+    'Wrong answers only!\nWhat is a sofware engineer?\n👇🏾👇🏿👇🏻👇🏽👇🏼',
+    'Wrong answers only!\nWhat is @StackOverflow?\n👇🏼👇🏾👇🏿👇🏻👇🏽',
+    'Wrong answers only!\nWhat is @GitHub?\n👇🏽👇🏼👇🏾👇🏿👇🏻',
+    'Wrong answers only!\nWhat is @GitLab?\n👇🏻👇🏽👇🏼👇🏾👇🏿',
+    'Wrong answers only!\nWhat is @ThePracticalDev?\n👇🏿👇🏽👇🏼👇🏾👇🏿',
+    'Wrong answers only!\nWhat is a CSS?\n👇🏾👇🏿👇🏻👇🏽👇🏼',
+    'Fill in the blanks:\nRoses are red 🌹\nViolets are blue\n________________\n________________',
+    'Haiku time! Give us your best coding haiku\nreminder: 3 lines, 5-7-5 \n\ninspired by @bendhalpern https://dev.to/ben/give-me-your-best-programming-haiku-54db',
+    ]
