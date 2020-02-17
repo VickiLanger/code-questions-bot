6726fb639e9cb069e34f8592294bcd7952cd00ef
Vicki <vicki.langer@statalog.org>
2020-02-17 11:33:03 -0500

    Add handles and hashtags

--- questions_pop_culture.py
+++ questions_pop_culture.py
@@ -8,9 +8,10 @@ Vicki Langer (@vicki_langer)
 # NOTE: please add the question, then comment the expected answer
 
 pop_culture_questions = [
-    'In "Ralph Breaks the Internet", what language was on Arthur\'s Heads Up Display (HUD)?',  # https://www.reddit.com/r/Python/comments/ariypt/just_noticed_that_the_code_in_ralph_breaks_the/
-    'In "Stranger Things" Season 2, what language was used?',  # BASIC https://moviecode.tumblr.com/post/166870949505/at-least-they-tried-basic-from-stranger-things
-    'In "Doctor Who" Season 7 Episode 2, what code is guiding the missiles?',  # In Doctor Who S07E02, missiles are being guided by the power of SVGs.
-    'In "Silicon Valley" Season 3 Episode 1, what language is being used?',  # https://www.reddit.com/r/SiliconValleyHBO/comments/4gbra7/i_got_the_silicon_valley_s03e01_code_to_compile/)
-    'What day is celebrated as Programmer\'s day? Why?',  #
+    'In "Ralph Breaks the Internet", what language was on Arthur\'s Heads Up Display (HUD)?\n@wreckitralph #RalphBreaksTheInternet',  # https://www.reddit.com/r/Python/comments/ariypt/just_noticed_that_the_code_in_ralph_breaks_the/
+    'In "Stranger Things" Season 2, what language was used?\n@Stranger_Things @stnetflixtv',  # BASIC https://moviecode.tumblr.com/post/166870949505/at-least-they-tried-basic-from-stranger-things
+    'In "Doctor Who" Season 7 Episode 2, what code is guiding the missiles?\n@bbcdoctorwho @DrWhoOnline',  # In Doctor Who S07E02, missiles are being guided by the power of SVGs.
+    'In "Silicon Valley" Season 3 Episode 1, what language is being used?\n@SiliconHBO',  # https://www.reddit.com/r/SiliconValleyHBO/comments/4gbra7/i_got_the_silicon_valley_s03e01_code_to_compile/)
+    'What day is celebrated as Programmer\'s day? Why?\n#ProgrammersDay',  #
+    #'something... \n@HaltAMC #HaltandCatchFire',  #
     ]
