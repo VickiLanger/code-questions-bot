52a3752b7a075e3fb30fee8c03f2b3a448c08a4f
Vicki <vicki.langer@statalog.org>
2020-02-18 14:04:43 -0500

    more questions and fun

--- questions_personal.py
+++ questions_personal.py
@@ -8,21 +8,24 @@ Vicki Langer (@vicki_langer)
 # NOTE: opinions welcome here
 
 personal_questions = [
-    'How important are math skills for software development?',  # inspired by https://dev.to/ben/how-important-are-math-skills-for-software-development-347n
-    'What is the single best code/programming book you\'ve read?',
+    '🧮 How important are math skills for software development?',  # inspired by https://dev.to/ben/how-important-are-math-skills-for-software-development-347n
+    '📚 What is the single best code/programming book you\'ve read?',
     'Tabs, Spaces, tabs as spaces, or spaces as tabs?',
-    'Light mode or dark mode? Why?',
-    'What are the tools you couldn\'t work without?',
-    'What does your desk or workspace look like today? \nbonus points for pics',
+    '⚪️ Light mode or ⚫️ dark mode? Why?',
+    '🛠️ What are the tools you couldn\'t work without?',
+    '🖥️ What does your desk or workspace look like today? \nbonus points for pics',
+    '🏫 How did you learn to code? School, Bootcamp, and/or self-taught? \nIf you could go back in time, would you do it the same way or differently?',
     'What does your editor/IDE look like?',  # inspired by https://dev.to/emmabostian/what-does-your-ide-code-editor-look-like-550e
     'What\'s the best learning resource you\'ve used? Why?',
     'What editor or IDE do you use? Any special reason?',
-    'Answer with a GIF. \nHow are you feeling about your code, right now?',
+    'Answer with a GIF. \nRight now, how are you feeling about your code?',
     'How did you get into coding/programming?',
-    'What is one place you\'d want to work?',
-    'What movies do you enjoy that have some sort of coding?',
-    'What books do you enjoy that have some sort of coding?',
-    'What shows do you enjoy that have some sort of coding?',
+    'What is one place you\'d LOVE want to work? Why?',
+    'What is one place you would NEVER want to work? Why?',
+    '📽️ What movies do you enjoy that have some sort of coding?',
+    '📕 What books do you enjoy that have some sort of coding?',
+    '🎙️ What podcasts do you enjoy that have some sort of coding?',
+    '📺 What shows do you enjoy that have some sort of coding?',
     'What was something that was really hard for you to understand, but isn\'t so bad now?',
     'What awesome humans are you following that everyone else should follow? \nWhy? \nTag \'em',
     'What badass women are you following that everyone else should follow? \nWhy? \nTag \'em',  # includes anyone cis, trans*, femme presenting, not femme presenting
@@ -33,15 +36,34 @@ personal_questions = [
     'What badass Indigenous folks are you following that everyone else should follow? \nWhy? \nTag \'em',
     # 'What badass underrepresented peeps are you following that everyone else should follow? \nWhy? \nTag \'em',
     'What badass folks are you following that everyone else should follow? \nWhy? \nTag \'em',
-    'Do you have a portfolio? Why or why not? \nShare a link\n👇🏼👇🏾👇🏽👇🏿👇🏻'
+    'Do you have a portfolio? Why or why not? \nShare a link\n👇🏽👇🏿👇🏼👇🏾👇🏻'
+    'Do you make stickers? Which is your favorite? \nShare a link\n👇🏽👇🏿👇🏼👇🏾👇🏻'
+    'Stickers on your laptop? \nShow \'em off\n👇🏾👇🏼👇🏽👇🏿👇🏻'
     'Do you write on dev.to? \nShare a link to your favorite article \n#DEVcommunity @ThePracticalDev',
-    'What\'s the weirdest bug you\'ve dealt with? \nTell us about it.',
+    '🐛🐞 What\'s the weirdest bug you\'ve dealt with? \nTell us about it.',
     'What\'s your go to framework? Why?',
     'What was your latest win?',
+    'Does coding spark joy? 🌟',
+    'Are you a #CodeNewbie or a Code Expert?',
     # 'What do you need help with? \nor \nWhat can you help someone with?',
     'What are your goals for the next few days?',
     'What\'s your browser of choice? Why?',
     'Describe you job with just a GIF!',
+    'What\'s your most abused emojis?',
+    'What\'s the last thing you messed up? \nOn a scale of, a Typo to Causing Earth to Implode, how bad was it?',
+    'Are you stressing about things outside of your control?',  # inspired by https://twitter.com/IndieHackers/status/1221888862441672712?s=20
+    'What are you going to get done this week?',  # inspired by https://twitter.com/IndieHackers/status/1219352147566956545?s=20
+    'What do you need help with right now?',  # inspired by https://twitter.com/IndieHackers/status/1217143340732440576?s=20
+    'What are you focusing on this week?',  # inspired by https://twitter.com/IndieHackers/status/1216742456651329538?s=20
+    'What are you learning right now?',  # inspired by https://twitter.com/IndieHackers/status/1216086124436885508?s=20
+    'How many hours do you typically work a day?',  # inspired by https://twitter.com/IndieHackers/status/1213157078241595392?s=20
+    'What\'s your preferred stack?',  # inspired by https://twitter.com/IndieHackers/status/1222868097981390848?s=20
+    'Describe your last month with a GIF...',  # inspired by https://twitter.com/IndieHackers/status/1224334070076801024?s=20
+    'How is your job changing the world? 🌍🌏🌎',  # inspired by https://twitter.com/IndieHackers/status/1225439760510001153?s=20
+    'How is your side project changing the world? 🌍🌏🌎',  # inspired by https://twitter.com/IndieHackers/status/1225439760510001153?s=20
+    'Are you stretching yourself too thin?',  # inspired by https://twitter.com/IndieHackers/status/1225926718491418624?s=20
+    'How do you pace yourself?',  # inspired by https://twitter.com/IndieHackers/status/1229698571311755264?s=20
+    'What\'s a stupid thing you\'ve done recently?',  # inspired by https://twitter.com/IndieHackers/status/1229499007032053760?s=20
     'Explain to a 5-year-old, what do you do for work?',
     'In emojis, what do you do for work?',
     'Do you work with Open Source? What project(s)?',
