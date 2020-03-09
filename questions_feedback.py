07888763e6f5c64b17edd4b1f4995a28306a5e7e
Vicki <vicki.langer@statalog.org>
2020-03-09 06:34:44 -0400

    moar questions!!!

--- questions_feedback.py
+++ questions_feedback.py
@@ -10,7 +10,8 @@ Vicki Langer (@vicki_langer)
 feedback_questions = [
     'What questions would you like us to ask? Or what kinds of questions would you like to see?',
     'It would be cool if @CodeQBot could _______________________ \nfill in the blank',
-    # 'Right now, I post 4 times a day. Should I post more? Should I post less?',
+    'Right now, I post 4 times a day. Should I post more? Should I post less?',
     'What time would you prefer to see our questions??',
+    'What time would be best for me to post questions? What timezone are you in?',
     # 'Right now, I post at 0330, 0930, 1530, and 2130 EST.',
     ]
