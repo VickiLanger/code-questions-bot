99217d09c368e57fcaffd0529524e94118baf510
Vicki <vicki.langer@statalog.org>
2020-03-12 13:30:27 -0400

    another movie question

--- questions_pop_culture.py
+++ questions_pop_culture.py
@@ -8,6 +8,7 @@ Vicki Langer (@vicki_langer)
 # NOTE: please add the question, then comment the expected answer
 
 pop_culture_questions = [
+    'What shows/movies have coders in them? So far, I\'ve heard of @wreckitralph @Stranger_Things @bbcdoctorwho @SiliconHBO @whoismrrobot @GoodTrouble @HaltAMC @ZoeysPlaylist @Devs_FXonHulu',
     'In "Ralph Breaks the Internet", what language was on Arthur\'s Heads Up Display (HUD)?\n@wreckitralph #RalphBreaksTheInternet https://preview.redd.it/mzjl2prvh3h21.png?width=960&crop=smart&auto=webp&s=d656833905a9a21ac44df1fe6a17ecd12b07bd41',  # https://www.reddit.com/r/Python/comments/ariypt/just_noticed_that_the_code_in_ralph_breaks_the/
     'In "Stranger Things" Season 2, what language was used?\n@Stranger_Things @stnetflixtv https://66.media.tumblr.com/55ae5b88098c062fbc52f9fc0d96647f/tumblr_oyisgcMzNg1tognpro1_640.jpg',  # BASIC https://moviecode.tumblr.com/post/166870949505/at-least-they-tried-basic-from-stranger-things
     'In "Doctor Who" Season 7 Episode 2, what code is guiding the missiles?\n@bbcdoctorwho @DrWhoOnline',  # In Doctor Who S07E02, missiles are being guided by the power of SVGs.
