c428686ddc96ae02fe17c9447a5c996e84ac15b4
Vicki <46823403+VickiLanger@users.noreply.github.com>
2021-08-03 17:49:12 -0400

    Spelling and grammar

--- questions_dir/questions_algorithms.py
+++ questions_dir/questions_algorithms.py
@@ -16,14 +16,14 @@ fun_questions = [
     'What does a binary tree look like?',
     'Where are binary trees used in production?',
     'Who is using binary trees outside of code interviews?',
-    'Hownwould you traverse a binary tree?',
+    'How would you traverse a binary tree?',
     
     # bubble sort
     'Why would you use bubble sort?',
     'When is it best to use a bubble sort?',
     'How does a bubble sort work?',
     'What does a bubble sort look like?',
-    'Where are bubble sort used in production?',
+    'Where are bubble sorts used in production?',
     'Who is using bubble sort outside of code interviews?',
   
 ]
