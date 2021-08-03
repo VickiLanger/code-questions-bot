fa358537f30e6750b5dc8ebcc5a999cb39261076
Vicki <46823403+VickiLanger@users.noreply.github.com>
2021-08-03 11:03:16 -0400

    Add algorithm questions

---
+++ questions_dir/questions_algorithms.py
@@ -0,0 +1,29 @@
+'''
+questions_algorithms.py: algorithm questions for @CodeQBot
+3 August 2021
+Vicki Langer (@vicki_langer)
+'''
+
+# TODO: add more questions
+# NOTE: what/when/why/where/how of typical algorithms and other questions about algorithms
+
+fun_questions = [
+    
+    #binary search trees
+    'Why would you use a binary tree?',
+    'When is it best to use a binay tree?',
+    'How does a binary tree work?',
+    'What does a binary tree look like?',
+    'Where are binary trees used in production?',
+    'Who is using binary trees outside of code interviews?',
+    'Hownwould you traverse a binary tree?',
+    
+    # bubble sort
+    'Why would you use bubble sort?',
+    'When is it best to use a bubble sort?',
+    'How does a bubble sort work?',
+    'What does a bubble sort look like?',
+    'Where are bubble sort used in production?',
+    'Who is using bubble sort outside of code interviews?',
+  
+]
